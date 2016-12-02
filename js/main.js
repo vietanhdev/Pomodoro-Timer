febc90ec11552766fe6daeae39a8f167727f517b
Nguyen Viet Anh <Nguyen Viet Anh>
2016-12-02 23:18:38 +0700

    add shortcut keys

--- js/main.js
+++ js/main.js
@@ -17,11 +17,9 @@ $('document').ready(function() {
     // Setup shortcut keys
     $(document).keypress(function(e) {
       if(e.charCode == 32) {
-        $('#start-btn').addClass('main-btn-pressed').delay(800).removeClass('main-btn-pressed');
         startTimer();
       }
       if(e.charCode == 115) {
-        $('#stop-btn').addClass('main-btn-pressed').delay(800).removeClass('main-btn-pressed');
         stopTimer();
       }
     });
